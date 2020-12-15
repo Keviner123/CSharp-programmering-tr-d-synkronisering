955706147e26d493f2f0db579768169ac354a0c9
My Name <my-name@chromium.org>
2020-12-15 13:40:39 +0100

    .

---
+++ Opgave1/Opgave1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    class Program
+    {
+        static int counter;
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(AddTwo);
+            Thread thread2 = new Thread(SubtractOne);
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.ReadLine();
+        }
+
+        static void AddTwo()
+        {
+            while(true)
+            {
+                lock ((object)counter)
+                {
+                    counter += 2;
+                    Console.WriteLine(counter);
+
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void SubtractOne()
+        {
+            while(true)
+            {
+                lock ((object)counter)
+                {
+                    counter += -1;
+                    Console.WriteLine(counter);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Opgave2_3/Opgave2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Opgave2
+{
+    class Program
+    {
+        static object locker = new object();
+        static int counter;
+
+        static void Main(string[] args)
+        {
+  
+                Thread thread1 = new Thread(PrintStars);
+                Thread thread2 = new Thread(PrintOctothorp);
+
+                thread1.Start();
+                thread2.Start();
+
+                Console.ReadKey();
+        }
+
+        static void PrintStars()
+        {
+            while (true)
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("*");
+                    }
+                    counter += 60;
+                    Console.WriteLine(" {0}", counter);
+                    Thread.Sleep(5);
+                }
+            }
+        }
+
+        static void PrintOctothorp()
+        {
+            while (true)
+            {
+                lock (locker)
+                {
+                    for (int i = 0; i < 60; i++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine(" {0}", counter);
+                    Thread.Sleep(5);
+                }
+            }
+        }
+    }
+}
